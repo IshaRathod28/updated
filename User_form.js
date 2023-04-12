2ef1f184b9d6643ebde255fe696cbaaf00cc9123
IshaRathod28 <124258565+IshaRathod28@users.noreply.github.com>
2023-04-12 17:26:06 +0530

    Update User_form.js

--- User_form.js
+++ User_form.js
@@ -322,3 +322,137 @@ function allLetter(inputtxt2)
 };
 
 export default User_form;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+CSE 154: Web Programming
+Node.js/Express “Cheat Sheet”
+This reference summarizes the most useful methods/properties used in CSE 154 for Node.js/Express. It is not an
+exhaustive reference for everything in Node.js/Express (for example, there exist many more fs
+methods/properties than are shown below), but provide most functions/properties you will be using in this class.
+Note that this Cheat Sheet is more comprehensive than the one provided in CSE154 exams.
+Basic Node.js Project Structure
+example-node-project/
+.gitignore
+APIDOC.md
+app.js
+node_modules/
+...
+package.json
+public/
+img/
+...
+index.html
+index.js
+styles.css
+Example Express app Template
+"use strict";
+/* File comment */
+const express = require("express");
+// other modules you use
+// program constants
+const app = express();
+// if serving front-end files in public/
+app.use(express.static(“public”));
+// if handling different POST formats
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(multer().none());
+// app.get/app.post endpoints
+// helper functions
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
+npm Commands
+Command Description
+npm init Initializes a node project. Creates packages.json to track required
+modules/packages, as well as the node_modules folder to track
+imported packages.
+npm install Installs any requirements for the local node package based on the
+contents of package.json.
+npm install <package-name> Installs a package from NPM’s own repository as well as any
+requirements specified in that package’s package.json file.
+CSE 154 Node.js Cheat Sheet Summer 2019 - Version 08/21/19
+Glossary
+Term Definition
+API Application Programming Interface.
+Web Service A type of API that supports HTTP Requests, returning data such as
+JSON or plain text.
+Express A module for simplifying the http-server core module in Node to
+implement APIs
+npm The Node Package Manager. Used to initialize package.json files
+and install Node project dependencies.
+Module A standalone package which can be used to extend the
+functionality of a Node project.
+Package Any project with a package.json file.
+API Documentation A file detailing the endpoints, usage, and functionality of various
+endpoints of an API.
+Server A publicly accessible machine which exchanges information with
+one or more clients at a time.
+Client A private or public machine which requests information from a
+server.
+Useful Core Modules
+Module Description
+fs The “file system” module with various functions to process data in the file system.
+path Provides functions to process path strings.
+util Provides various “utility” functions, such as util.promisify.
+CSE 154 Node.js Cheat Sheet Summer 2019 - Version 08/21/19
+Other Useful Modules
+The following modules must be installed for each new project using npm install <module-name>.
+Module Description
+express A module for simplifying the http-server core module in Node to implement APIs.
+glob Allows for quick traversal and filtering of files in a complex directory.
+multer Used to support FormData POST requests on the server-side so we can access the req.body parameters.
+mysql Provides functionality for interacting with a database and tables.
+promise-mysql Promisified wrapper over mysql module - each function in the mysql module returns a promise instead of
+taking a callback as the last argument (recommended).
+cookie-parser A module to access cookies with req/res objects in Express.
+Express Route Functions
+Function Description
+app.get(“path”, middlewareFn(s));
+app.get(“/”, (req, res) => {
+...
+});
+app.get(“/:city”, (req, res) => {
+let city = req.params.city;
+...
+});
+app.get(“/cityData”, (req, res) => {
+let city = req.query.city;
+...
+});
+// Example with multiple middleware functions
+app.get(“/”, validateInput, (req, res) => {
+...
+}, handleErr);
+Defines a server endpoint which accepts a valid GET request.
+Request and response objects are passed as req and res
+respectively. Path parameters can be specified in path with
+“:varname” and accessed via req.params. Query parameters
+can be accessed via req.query.
+app.post(“path”, middlewareFn(s));
+app.post(“/addItem”, (req, res) => {
+let itemName = req.body.name;
+...
+}
+Defines a server endpoint which accepts a valid POST request.
+Request and response objects are passed as req and res
+respectively. POST body is accessible via req.body. Requires
+POST middleware and multer module for FormData POST
+requests.
+CSE 154 Node.js Cheat Sheet Summer 2019 - Version 08/21/19
+
